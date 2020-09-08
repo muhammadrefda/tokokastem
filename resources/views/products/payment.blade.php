a212c36074c406b547862d5c618e2b743354b23a
refda <muhammadrefda@gmail.com>
2020-09-08 14:53:22 +0700

    update payment detail page

--- resources/views/products/payment.blade.php
+++ resources/views/products/payment.blade.php
@@ -59,39 +59,51 @@
                                                         <div class="row">
                                                             <div class="col-md-12">
                                                                 <div class="aa-checkout-single-bill">
-                                                                    <textarea cols="8" rows="3">Alamat*</textarea>
+                                                                    <p>
+                                                                        contoh penulisan alamat:
+                                                                    </p>
+                                                                    <p>
+                                                                        Jl. Muhammad Kahfi 2, Rt 03/08 no.38,
+                                                                        DKI Jakarta, Jakarta Selatan, Jagakarsa 12640
+                                                                        Indonesia
+                                                                    </p>
+                                                                    <textarea cols="8" rows="3">Alamat Lengkap*</textarea>
                                                                 </div>
                                                             </div>
                                                         </div>
                                                         <div class="row">
-                                                            <div class="col-md-6">
-                                                                <div class="aa-checkout-single-bill">
-                                                                    <input type="text" placeholder="Provinsi">
-                                                                </div>
-                                                            </div>
-                                                            <div class="col-md-6">
-                                                                <div class="aa-checkout-single-bill">
-                                                                    <input type="text" placeholder="Kota">
-                                                                </div>
-                                                            </div>
+{{--                                                            <div class="col-md-6">--}}
+{{--                                                                <div class="aa-checkout-single-bill">--}}
+{{--                                                                    <input type="text" placeholder="Provinsi">--}}
+{{--                                                                </div>--}}
+{{--                                                            </div>--}}
+{{--                                                            <div class="col-md-6">--}}
+{{--                                                                <div class="aa-checkout-single-bill">--}}
+{{--                                                                    <input type="text" placeholder="Kota">--}}
+{{--                                                                </div>--}}
+{{--                                                            </div>--}}
                                                         </div>
                                                         <div class="row">
-                                                            <div class="col-md-6">
-                                                                <div class="aa-checkout-single-bill">
-                                                                    <input type="text" placeholder="Kabupaten">
-                                                                </div>
-                                                            </div>
-                                                            <div class="col-md-6">
-                                                                <div class="aa-checkout-single-bill">
-                                                                    <input type="text" placeholder="Kode Pos">
-                                                                </div>
-                                                            </div>
+{{--                                                            <div class="col-md-6">--}}
+{{--                                                                <div class="aa-checkout-single-bill">--}}
+{{--                                                                    <input type="text" placeholder="Kabupaten">--}}
+{{--                                                                </div>--}}
+{{--                                                            </div>--}}
+{{--                                                            <div class="col-md-6">--}}
+{{--                                                                <div class="aa-checkout-single-bill">--}}
+{{--                                                                    <input type="text" placeholder="Kode Pos">--}}
+{{--                                                                </div>--}}
+{{--                                                            </div>--}}
                                                         </div>
                                                         <div class="row">
                                                             <div class="col-md-12">
                                                                 <div class="aa-checkout-single-bill">
                                                                     <textarea cols="8" rows="3">Catatan</textarea>
                                                                 </div>
+                                                                <label for="">Silahkan screenshot halaman ini dan upload ke tombol bayar dibawah</label>
+                                                                <br>
+                                                                <a class="btn btn-success"
+                                                                   href="https://api.whatsapp.com/send?phone=81411165221&text=Saya%20ingin%20melakukan%20konfirmasi%20pembayaran">Bayar</a>
                                                             </div>
                                                         </div>
                                                     </div>
@@ -125,12 +137,12 @@
                                                 </tfoot>
                                             </table>
                                         </div>
-                                        <h4>Payment Method</h4>
-                                        <div class="aa-payment-method">
-                                            <label for="">Silahkan screenshot halaman ini dan upload ke tombol bayar dibawah</label>
-                                            <a class="btn btn-success" href="https://api.whatsapp.com/send?phone=81411165221&text=Saya%20ingin%20melakukan%20konfirmasi%20pembayaran">Bayar</a>
+{{--                                        <h4>Payment Method</h4>--}}
+{{--                                        <div class="aa-payment-method">--}}
+{{--                                            <label for="">Silahkan screenshot halaman ini dan upload ke tombol bayar dibawah</label>--}}
+{{--                                            <a class="btn btn-success" href="https://api.whatsapp.com/send?phone=81411165221&text=Saya%20ingin%20melakukan%20konfirmasi%20pembayaran">Bayar</a>--}}
 
-                                        </div>
+{{--                                        </div>--}}
                                         <br>
 
                                     </div>
