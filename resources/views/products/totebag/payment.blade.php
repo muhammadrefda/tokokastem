37a6f0b1a6e3c698a0e85e56bd75779a1c8fe3bf
refda <muhammadrefda@gmail.com>
2020-11-27 17:37:46 +0700

    WIP: create detail_pengiriman page for totebag and adjust payment page for totebag

--- resources/views/products/totebag/payment.blade.php
+++ resources/views/products/totebag/payment.blade.php
@@ -6,18 +6,17 @@
     <section id="checkout" >
         <div class="container">
             <div class="row">
-
-                <div class="col-md-6">
+                <div class="col-md-12">
                     <div class="checkout-left">
                         <div class="aa-order-summary-area">
-                            <h4>Rincian Pesanan</h4>
+                            <br>
+                            <h2>Rincian Pesanan</h2>
                             <table class="table table-responsive">
                                 <thead>
                                 <tr>
                                     <th>Produk</th>
                                     <th>Qty </th>
                                     <th>Harga Satuan</th>
-
                                 </tr>
                                 </thead>
                                 <tbody>
@@ -27,7 +26,6 @@
                                         <td>{{$td->quantity}}</td>
                                         <td>{{number_format($td->price_totebag)}}</td>
                                     </tr>
-
                                 </tbody>
                                 @endforeach
                                 <tfoot>
@@ -36,19 +34,28 @@
                                     <td><b>{{$td->unique_code}}</b></td>
                                 </tr>
                                 <tr>
+                                    <th colspan="2">Ongkir</th>
+                                    <td>
+                                        Rp . {{ number_format($ongkir) }}
+                                    </td>
+                                </tr>
+                                <tr>
                                     <th colspan="2">Total</th>
                                     <td>
-                                        Rp. {{number_format($td->total)}}
+                                        Rp. {{number_format($td->total+$ongkir)}}
                                     </td>
                                 </tr>
                                 </tfoot>
                             </table>
                         </div>
-                        <h4>setelah melakukan transfer, silahkan mengirim bukti pembayaran ke nomor berikut</h4>
                         <div class="aa-order-summary-area">
-                            <a class="" href="https://api.whatsapp.com/send?phone=81411165221&text=Saya%20ingin%20melakukan%20konfirmasi%20pembayaran"><b>0812345678976</b></a>
+                            <h4>Setelah melakukan transfer, silahkan mengirim bukti pembayaran ke nomor berikut</h4>
+                            <div class="col-md-offset-10">
+                                <a class="btn btn-primary" style="background-color: #F36365; color: #ffffff;" href="https://api.whatsapp.com/send?phone=81411165221&text=Saya%20ingin%20melakukan%20konfirmasi%20pembayaran"><b>0812345678976</b></a>
+                            </div>
                         </div>
                     </div>
+
                 </div>
 
                 {{--                <div class="col-md-6">--}}
