08d435c549ad05de82011768d7ad2c9e8ebf0006
refda <muhammadrefda@gmail.com>
2020-12-25 00:37:45 +0700

    Feature: Adjust "cara pesan page"

--- resources/views/cara-pesan.blade.php
+++ resources/views/cara-pesan.blade.php
@@ -1,35 +1,112 @@
 @extends('layouts.master')
 @section('title','Cara Pesan')
-
 @section('content')
-
-    <!-- Blog Archive -->
     <section id="aa-blog-archive">
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
                     <div class="aa-blog-archive-area">
+                        <h2>Bagaimana Cara Belanja di TokoKastem?</h2>
+
                         <div class="row">
                             <div class="col-md-9">
                                 <!-- Blog details -->
                                 <div class="aa-blog-content aa-blog-details">
                                     <article class="aa-blog-content-single">
-                                        <h2><a href="#">step 1</a></h2>
+                                        <h2><a href="#">Langkah 1</a></h2>
                                         <figure class="aa-blog-img">
-                                            <a href="#"><img src="img/fashion/3.jpg" alt="fashion img"></a>
+                                            <a href="#"><img src="{{asset('img/cara-pesan/homepage.png')}}" alt="halaman awal"></a>
                                         </figure>
                                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor omnis ipsam, nisi voluptate reprehenderit consectetur, illum possimus asperiores. Sed ad natus saepe, distinctio veniam? Error natus, dolore rem beatae, dolorum, aliquid sapiente ipsa voluptatum impedit vel maiores nobis totam! Ad dicta obcaecati dolore natus deleniti qui, hic animi, nobis cumque fuga non sapiente neque voluptatum nisi perspiciatis, molestiae vero distinctio officia, laboriosam. Veritatis assumenda nam est fuga rem asperiores repellat veniam magnam, molestias iusto quas facilis, et eaque. Est magni voluptas quibusdam saepe quis laudantium atque maxime itaque optio ipsam qui voluptates beatae, perspiciatis fugiat tempora maiores, odio, sed non!</p>
-
                                     </article>
                                     <article class="aa-blog-content-single">
-                                        <h2><a href="#">step 2</a></h2>
+                                        <h2><a href="#">Langkah 2</a></h2>
                                         <figure class="aa-blog-img">
-                                            <a href="#"><img src="img/fashion/3.jpg" alt="fashion img"></a>
+                                            <a href="#"><img src="{{asset('img/cara-pesan/login.png')}}" alt="halaman login"></a>
                                         </figure>
                                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor omnis ipsam, nisi voluptate reprehenderit consectetur, illum possimus asperiores. Sed ad natus saepe, distinctio veniam? Error natus, dolore rem beatae, dolorum, aliquid sapiente ipsa voluptatum impedit vel maiores nobis totam! Ad dicta obcaecati dolore natus deleniti qui, hic animi, nobis cumque fuga non sapiente neque voluptatum nisi perspiciatis, molestiae vero distinctio officia, laboriosam. Veritatis assumenda nam est fuga rem asperiores repellat veniam magnam, molestias iusto quas facilis, et eaque. Est magni voluptas quibusdam saepe quis laudantium atque maxime itaque optio ipsam qui voluptates beatae, perspiciatis fugiat tempora maiores, odio, sed non!</p>
+                                    </article>
 
+                                    <article class="aa-blog-content-single">
+                                        <h2><a href="#">Langkah 3</a></h2>
+                                        <figure class="aa-blog-img">
+                                            <a href="#"><img src="{{asset('img/cara-pesan/register.png')}}" alt="halaman register"></a>
+                                        </figure>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor omnis ipsam, nisi voluptate reprehenderit consectetur, illum possimus asperiores. Sed ad natus saepe, distinctio veniam? Error natus, dolore rem beatae, dolorum, aliquid sapiente ipsa voluptatum impedit vel maiores nobis totam! Ad dicta obcaecati dolore natus deleniti qui, hic animi, nobis cumque fuga non sapiente neque voluptatum nisi perspiciatis, molestiae vero distinctio officia, laboriosam. Veritatis assumenda nam est fuga rem asperiores repellat veniam magnam, molestias iusto quas facilis, et eaque. Est magni voluptas quibusdam saepe quis laudantium atque maxime itaque optio ipsam qui voluptates beatae, perspiciatis fugiat tempora maiores, odio, sed non!</p>
+                                    </article>
+                                    <article class="aa-blog-content-single">
+                                        <h2><a href="#">Langkah 4</a></h2>
+                                        <figure class="aa-blog-img">
+                                            <a href="#"><img src="{{asset('img/cara-pesan/product section.png')}}" alt="produk"></a>
+                                        </figure>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor omnis ipsam, nisi voluptate reprehenderit consectetur, illum possimus asperiores. Sed ad natus saepe, distinctio veniam? Error natus, dolore rem beatae, dolorum, aliquid sapiente ipsa voluptatum impedit vel maiores nobis totam! Ad dicta obcaecati dolore natus deleniti qui, hic animi, nobis cumque fuga non sapiente neque voluptatum nisi perspiciatis, molestiae vero distinctio officia, laboriosam. Veritatis assumenda nam est fuga rem asperiores repellat veniam magnam, molestias iusto quas facilis, et eaque. Est magni voluptas quibusdam saepe quis laudantium atque maxime itaque optio ipsam qui voluptates beatae, perspiciatis fugiat tempora maiores, odio, sed non!</p>
                                     </article>
                                 </div>
+                            </div>
+                            <div class="col-md-6">
+                                <article class="aa-blog-content-single">
+                                    <h2><a href="#">Langkah 5</a></h2>
+                                    <figure class="aa-blog-img">
+                                        <a href="#"><img src="{{asset('img/cara-pesan/fabric/detail-product-fabric.png')}}" alt="produk"></a>
+                                    </figure>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor omnis ipsam, nisi voluptate reprehenderit consectetur, illum possimus asperiores. Sed ad natus saepe, distinctio veniam? Error natus, dolore rem beatae, dolorum, aliquid sapiente ipsa voluptatum impedit vel maiores nobis totam! Ad dicta obcaecati dolore natus deleniti qui, hic animi, nobis cumque fuga non sapiente neque voluptatum nisi perspiciatis, molestiae vero distinctio officia, laboriosam. Veritatis assumenda nam est fuga rem asperiores repellat veniam magnam, molestias iusto quas facilis, et eaque. Est magni voluptas quibusdam saepe quis laudantium atque maxime itaque optio ipsam qui voluptates beatae, perspiciatis fugiat tempora maiores, odio, sed non!</p>
+                                </article>
+                            </div>
+                            <div class="col-md-6">
+                                <article class="aa-blog-content-single">
+                                    <h2><a href="#">Langkah 5.a</a></h2>
+                                    <figure class="aa-blog-img">
+                                        <a href="#"><img src="{{asset('img/cara-pesan/mask/design-masker-page.png')}}" alt="produk"></a>
+                                    </figure>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor omnis ipsam, nisi voluptate reprehenderit consectetur, illum possimus asperiores. Sed ad natus saepe, distinctio veniam? Error natus, dolore rem beatae, dolorum, aliquid sapiente ipsa voluptatum impedit vel maiores nobis totam! Ad dicta obcaecati dolore natus deleniti qui, hic animi, nobis cumque fuga non sapiente neque voluptatum nisi perspiciatis, molestiae vero distinctio officia, laboriosam. Veritatis assumenda nam est fuga rem asperiores repellat veniam magnam, molestias iusto quas facilis, et eaque. Est magni voluptas quibusdam saepe quis laudantium atque maxime itaque optio ipsam qui voluptates beatae, perspiciatis fugiat tempora maiores, odio, sed non!</p>
+                                </article>
+                            </div>
+                            <div class="col-md-6">
+                                <article class="aa-blog-content-single">
+                                    <h2><a href="#">Langkah 6</a></h2>
+                                    <figure class="aa-blog-img">
+                                        <a href="#"><img src="{{asset('img/cara-pesan/fabric/detail-shipping.png')}}" alt="produk"></a>
+                                    </figure>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor omnis ipsam, nisi voluptate reprehenderit consectetur, illum possimus asperiores. Sed ad natus saepe, distinctio veniam? Error natus, dolore rem beatae, dolorum, aliquid sapiente ipsa voluptatum impedit vel maiores nobis totam! Ad dicta obcaecati dolore natus deleniti qui, hic animi, nobis cumque fuga non sapiente neque voluptatum nisi perspiciatis, molestiae vero distinctio officia, laboriosam. Veritatis assumenda nam est fuga rem asperiores repellat veniam magnam, molestias iusto quas facilis, et eaque. Est magni voluptas quibusdam saepe quis laudantium atque maxime itaque optio ipsam qui voluptates beatae, perspiciatis fugiat tempora maiores, odio, sed non!</p>
+                                </article>
+                            </div>
+                            <div class="col-md-6">
+                                <article class="aa-blog-content-single">
+                                    <h2><a href="#">Langkah 6.a</a></h2>
+                                    <figure class="aa-blog-img">
+                                        <a href="#"><img src="{{asset('img/cara-pesan/mask/detail-shipping.png')}}" alt="produk"></a>
+                                    </figure>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor omnis ipsam, nisi voluptate reprehenderit consectetur, illum possimus asperiores. Sed ad natus saepe, distinctio veniam? Error natus, dolore rem beatae, dolorum, aliquid sapiente ipsa voluptatum impedit vel maiores nobis totam! Ad dicta obcaecati dolore natus deleniti qui, hic animi, nobis cumque fuga non sapiente neque voluptatum nisi perspiciatis, molestiae vero distinctio officia, laboriosam. Veritatis assumenda nam est fuga rem asperiores repellat veniam magnam, molestias iusto quas facilis, et eaque. Est magni voluptas quibusdam saepe quis laudantium atque maxime itaque optio ipsam qui voluptates beatae, perspiciatis fugiat tempora maiores, odio, sed non!</p>
+                                </article>
+                        </div>
+
+                            <div class="col-md-6">
+                                <article class="aa-blog-content-single">
+                                    <h2><a href="#">Langkah 7</a></h2>
+                                    <figure class="aa-blog-img">
+                                        <a href="#"><img src="{{asset('img/cara-pesan/fabric/detail-payment.png')}}" alt="produk"></a>
+                                    </figure>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor omnis ipsam, nisi voluptate reprehenderit consectetur, illum possimus asperiores. Sed ad natus saepe, distinctio veniam? Error natus, dolore rem beatae, dolorum, aliquid sapiente ipsa voluptatum impedit vel maiores nobis totam! Ad dicta obcaecati dolore natus deleniti qui, hic animi, nobis cumque fuga non sapiente neque voluptatum nisi perspiciatis, molestiae vero distinctio officia, laboriosam. Veritatis assumenda nam est fuga rem asperiores repellat veniam magnam, molestias iusto quas facilis, et eaque. Est magni voluptas quibusdam saepe quis laudantium atque maxime itaque optio ipsam qui voluptates beatae, perspiciatis fugiat tempora maiores, odio, sed non!</p>
+                                </article>
+                            </div>
+                            <div class="col-md-6">
+                                <article class="aa-blog-content-single">
+                                    <h2><a href="#">Langkah 7.a</a></h2>
+                                    <figure class="aa-blog-img">
+                                        <a href="#"><img src="{{asset('img/cara-pesan/mask/detail-payment.png')}}" alt="produk"></a>
+                                    </figure>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor omnis ipsam, nisi voluptate reprehenderit consectetur, illum possimus asperiores. Sed ad natus saepe, distinctio veniam? Error natus, dolore rem beatae, dolorum, aliquid sapiente ipsa voluptatum impedit vel maiores nobis totam! Ad dicta obcaecati dolore natus deleniti qui, hic animi, nobis cumque fuga non sapiente neque voluptatum nisi perspiciatis, molestiae vero distinctio officia, laboriosam. Veritatis assumenda nam est fuga rem asperiores repellat veniam magnam, molestias iusto quas facilis, et eaque. Est magni voluptas quibusdam saepe quis laudantium atque maxime itaque optio ipsam qui voluptates beatae, perspiciatis fugiat tempora maiores, odio, sed non!</p>
+                                </article>
+                            </div>
+
+                            <div class="col-md-9">
+                                <article class="aa-blog-content-single">
+                                    <h2><a href="#">Langkah 6</a></h2>
+                                    <figure class="aa-blog-img">
+                                        <a href="#"><img src="{{asset('img/cara-pesan/invoice.png')}}" alt="produk"></a>
+                                    </figure>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor omnis ipsam, nisi voluptate reprehenderit consectetur, illum possimus asperiores. Sed ad natus saepe, distinctio veniam? Error natus, dolore rem beatae, dolorum, aliquid sapiente ipsa voluptatum impedit vel maiores nobis totam! Ad dicta obcaecati dolore natus deleniti qui, hic animi, nobis cumque fuga non sapiente neque voluptatum nisi perspiciatis, molestiae vero distinctio officia, laboriosam. Veritatis assumenda nam est fuga rem asperiores repellat veniam magnam, molestias iusto quas facilis, et eaque. Est magni voluptas quibusdam saepe quis laudantium atque maxime itaque optio ipsam qui voluptates beatae, perspiciatis fugiat tempora maiores, odio, sed non!</p>
+                                </article>
                             </div>
                         </div>
                     </div>
@@ -37,5 +114,4 @@
             </div>
         </div>
     </section>
-    <!-- / Blog Archive -->
 @endsection
